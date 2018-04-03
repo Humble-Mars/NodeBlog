80501dd93e0afbe81cc496ad3eab2366a6c3619b
I348299 <mars.yang@sap.com>
2018-04-03 13:54:04 +0800

    initially commit

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,8 @@
+
 module.exports = function (app) {
   app.get('/', function (req, res) {
     res.redirect('/posts')
-  })
+  });
   app.use('/signup', require('./signup'));
   app.use('/signin', require('./signin'));
   app.use('/signout', require('./signout'));
@@ -9,10 +10,4 @@ module.exports = function (app) {
   app.use('/comments', require('./comments'));
 };
 
-  //// 404 page
-// app.use(function (req, res) {
-//     if (!res.headersSent) {
-//       res.status(404).render('404')
-//     }
-//   })
-// }
+
